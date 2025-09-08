9b31908aed5ec34592eecc82a41ebbbdb4775827
Diego Sagarnaga <diegosagarnaga@gmail.com>
2025-09-08 13:58:54 -0600

    Fix: importar include en urls.py y habilitar auth URLs

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 ﻿from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import RedirectView
 from core import views
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", RedirectView.as_view(url="/admin/", permanent=False)),
     path("accounts/login/", RedirectView.as_view(url="/admin/login/", permanent=False)),
     path("admin/", admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("t/<uuid:token>/", views.public_status, name="public_status"),
     path("t/<uuid:token>/qr.png", views.qr, name="qr"),
     path("recepcion/nueva-orden/", views.reception_new_order, name="reception_new_order"),
